48676275bf76ca3945ab06a182a9db1112e55961
alabi leke <w0838407@myscc.ca>
2025-02-20 17:31:23 -0500

    MADE THE COMPONENT

---
+++ src/app/hair-list-item/hair-list-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hair-list-item',
+  imports: [],
+  templateUrl: './hair-list-item.component.html',
+  styleUrl: './hair-list-item.component.css'
+})
+export class HairListItemComponent {
+
+}
---
+++ src/app/hair-list/hair-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hair-list',
+  imports: [],
+  templateUrl: './hair-list.component.html',
+  styleUrl: './hair-list.component.css'
+})
+export class HairListComponent {
+
+}
