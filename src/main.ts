339987337b777889a657f4aedcec0f0a440d5306
alabi leke <w0838407@myscc.ca>
2025-03-05 21:00:18 -0500

    fixed the route rediect

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,20 @@ import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
 import {HairListComponent} from "./app/hair-list/hair-list.component";
 import {HairListItemComponent} from "./app/hair-list-item/hair-list-item.component";
+import {ModifyHairProductItemComponent} from "./app/components/modify-hair-product-item/modify-hair-product-item.component";
+import {PageNotFoundComponent} from "./app/components/page-not-found/page-not-found.component";
 
 
 
 const routes: Routes =[
-  {path:'', redirectTo: '/students', pathMatch: 'full'},
+  {path:'', redirectTo: '/HairProduct', pathMatch: 'full'},
   {path: 'HairProduct', component:HairListComponent},
   {path: 'HairProduct/:id', component: HairListItemComponent},
-
+  {path: 'modify-hair-product', component: ModifyHairProductItemComponent},
+  {path:"**", component:PageNotFoundComponent}
 ]
-bootstrapApplication(AppComponent, appConfig)
+
+bootstrapApplication(AppComponent, {
+  providers:[provideRouter(routes)]
+})
   .catch((err) => console.error(err));
