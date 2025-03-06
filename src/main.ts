6d3a878f9186ea3a81647ba42b4c202ce85152c1
alabi leke <w0838407@myscc.ca>
2025-03-05 20:50:40 -0500

    added route definition in ain.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {HairListComponent} from "./app/hair-list/hair-list.component";
+import {HairListItemComponent} from "./app/hair-list-item/hair-list-item.component";
 
+
+
+const routes: Routes =[
+  {path:'', redirectTo: '/students', pathMatch: 'full'},
+  {path: 'HairProduct', component:HairListComponent},
+  {path: 'HairProduct/:id', component: HairListItemComponent},
+
+]
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
