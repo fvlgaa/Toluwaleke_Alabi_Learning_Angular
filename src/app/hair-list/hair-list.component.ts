6c9e7b57349894733d6a89413795875ba5b71721
alabi leke <w0838407@myscc.ca>
2025-03-12 23:32:18 -0400

    done with assignment 5

--- src/app/hair-list/hair-list.component.ts
+++ src/app/hair-list/hair-list.component.ts
@@ -1,36 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { HairProduct } from '../models/hair-product';
 import { HairProductService } from '../services/hair-product.service';
-import { Router } from '@angular/router';
+
+import { FormsModule } from "@angular/forms";
+import { HairListItemComponent } from "../hair-list-item/hair-list-item.component";
+import { NgClass, NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-hair-list',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    HairListItemComponent,
+    NgForOf,
+    NgClass
+  ],
   templateUrl: './hair-list.component.html',
-  styleUrls: ['./hair-list.component.css']
+  styleUrls: ['./hair-list.component.css']  // Corrected to 'styleUrls' (plural)
 })
 export class HairListComponent implements OnInit {
   hairList: HairProduct[] = [];
 
-  constructor(
-    private hairProductService: HairProductService,
-    private router: Router
-  ) { }
+  constructor(private hairProductService: HairProductService) { }
 
   ngOnInit(): void {
+    // Fetching data using the service
     this.hairProductService.getAllContent().subscribe(data => {
-      this.hairList = data;
-    });
-  }
-
-  onEdit(id: number): void {
-    this.router.navigate(['/modify', id]);
-  }
-
-  onDelete(id: number): void {
-    this.hairProductService.deleteHairProduct(id).subscribe(() => {
-      this.hairList = this.hairList.filter(hair => hair.id !== id);
+      this.hairList = data;  // Storing the fetched data
     });
   }
 }
+
