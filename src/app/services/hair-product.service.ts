26bfe21ebc9285dcc896c8b7a3b4803e9310154b
alabi leke <w0838407@myscc.ca>
2025-03-12 20:24:33 -0400

    updated the service to handle crud

--- src/app/services/hair-product.service.ts
+++ src/app/services/hair-product.service.ts
@@ -9,53 +9,22 @@ import { HairProduct } from "../models/hair-product";
 export class HairProductService {
   private hairProducts: HairProduct[] = hairList;
 
-  constructor() { }
+  constructor() {}
 
   getHairProducts(): Observable<HairProduct[]> {
     return of(this.hairProducts);
   }
 
-  getAllContent(): Observable<HairProduct[]> {
+  getHairProductById(id: number): Observable<HairProduct | undefined> {
+    return of(this.hairProducts.find(product => product.id === id));
+  }
+
+  addHairProduct(newProduct: HairProduct): Observable<HairProduct[]> {
+    this.hairProducts.push({ ...newProduct, id: this.hairProducts.length + 1 });
     return of(this.hairProducts);
   }
 
-  addHairProduct(newHairProduct: HairProduct): Observable<HairProduct[]> {
-    this.hairProducts.push(newHairProduct);
-    return of(this.hairProducts);
-  }
-
-  updateHairProduct(updatedHairProduct: HairProduct): Observable<HairProduct[]> {
-    const index = this.hairProducts.findIndex(product => product.id === updatedHairProduct.id);
-    if (index !== -1) {
-      this.hairProducts[index] = updatedHairProduct;
-    }
-    return of(this.hairProducts);
-  }
-
-  deleteHairProduct(hairProductId: number): Observable<HairProduct[]> {
-    const index = this.hairProducts.findIndex(product => product.id === hairProductId);
-    if (index !== -1) {
-      this.hairProducts.splice(index, 1);
-    }
-    return of(this.hairProducts);
-  }
-
-  getHairProductById(hairProductId: number): Observable<HairProduct | undefined> {
-    const foundProduct = this.hairProducts.find(product => product.id === hairProductId);
-    return of(foundProduct);
-  }
-
-  getHairProductByIdNumber(id: number): Observable<HairProduct | undefined> {
-    const foundProduct = this.hairProducts.find(product => product.id === id);
-    return of(foundProduct);
-  }
-
-  addHairProductAndReturnArray(newProduct: HairProduct): Observable<HairProduct[]> {
-    this.hairProducts.push(newProduct);
-    return of(this.hairProducts);
-  }
-
-  updateHairProductAndReturnArray(updatedProduct: HairProduct): Observable<HairProduct[]> {
+  updateHairProduct(updatedProduct: HairProduct): Observable<HairProduct[]> {
     const index = this.hairProducts.findIndex(product => product.id === updatedProduct.id);
     if (index !== -1) {
       this.hairProducts[index] = updatedProduct;
@@ -63,12 +32,13 @@ export class HairProductService {
     return of(this.hairProducts);
   }
 
-  removeHairProductByIdAndReturnRemoved(id: number): Observable<HairProduct | undefined> {
+  deleteHairProduct(id: number): Observable<HairProduct[]> {
+    this.hairProducts = this.hairProducts.filter(product => product.id !== id);
+    return of(this.hairProducts);
+  }
+
+  removeHairProductById(id: number): Observable<HairProduct | undefined> {
     const index = this.hairProducts.findIndex(product => product.id === id);
-    if (index !== -1) {
-      const removedProduct = this.hairProducts.splice(index, 1)[0];
-      return of(removedProduct);
-    }
-    return of(undefined);
+    return index !== -1 ? of(this.hairProducts.splice(index, 1)[0]) : of(undefined);
   }
 }
